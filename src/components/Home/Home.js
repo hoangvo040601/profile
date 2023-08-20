b26ead7bb315d4ddecb405c191e7cbd3548b391d
developer-abdulali <ab.ali.soomro@gmail.com>
2023-08-21 03:09:28 +0500

    updated home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
-import { Link } from "react-router-dom"; // Import Link from React Router
 import Home2 from "./Home2";
 import Type from "./Type";
 
@@ -29,11 +28,7 @@ function Home() {
               <div style={{ padding: 50, textAlign: "left" }}>
                 <Type />
               </div>
-              <div>
-                <Link className="downloadResumeBtn" to="/about">
-                Download resume
-                </Link>
-              </div>
+            
             </Col>
 
             <Col md={5} style={{ paddingBottom: 20 }}>
